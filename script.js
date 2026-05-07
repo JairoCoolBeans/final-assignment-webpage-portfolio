67a8ac4f18b726aaedcfa23be823620713b96094
JairoCoolBeans <jairomata1230@gmail.com>
2026-05-07 00:04:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ document.getElementById("darkModeBtn").onclick = function () {
   document.body.classList.toggle("dark");
 };
 
+const startBtn = document.getElementById("startBtn");
+const menu = document.getElementById("startMenu");
+
+startBtn.onclick = () => {
+  menu.style.display = menu.style.display === "block" ? "none" : "block";
+};
+
+
+const bookCard = document.getElementById("bookProject");
+const bookWindow = document.getElementById("bookWindow");
+const closeBook = document.getElementById("closeBook");
+
+// open window
+bookCard.addEventListener("click", () => {
+  bookWindow.style.display = "block";
+});
+
+// close window
+closeBook.addEventListener("click", () => {
+  bookWindow.style.display = "none";
+});
